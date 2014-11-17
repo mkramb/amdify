ca7a0aad49dbe02348ac914a2f3cdc8b84a6893d
Mitja Kramberger <mkramberger@atlassian.com>
2014-11-17 16:48:17 +1100

    stringent comparing

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ module.exports = function(source, key) {
   var namespace;
 
   rocambole.recursive(astree, function(node) {
-    if (node.type == 'CallExpression') {
+    if (node.type === 'CallExpression') {
       if (node.callee.toString() == key) {
         namespace = node.arguments[0].value
       }
     }
 
-    if (node.type == 'AssignmentExpression') {
+    if (node.type === 'AssignmentExpression') {
       if (node.left.toString() === namespace) {
         console.log(namespace, node.right.toString());
       }
